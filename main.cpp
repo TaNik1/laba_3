d8ea758759ba5e9f932084659e910ad360a17e1a
TaNik <nikitat612006@gmail.com>
2025-11-24 19:03:16 +0500

    new main.cpp from main branch + add first function

--- main.cpp
+++ main.cpp
@@ -2,11 +2,46 @@
 // Created by nikit on 24.11.2025.
 //
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Our names are:" << endl;
-    cout << "Nikita" << endl;
+// Функция №1
+void read_lines_from_file(const string& input_filename, vector<string>& lines)
+{
+    ifstream file(input_filename);
+    string line;
+
+    while (getline(file, line))
+    {
+        lines.push_back(line);
+    }
+}
+
+// Функция №2
+void print_lines_to_console(const vector<string>& lines)
+{
+
+}
+
+// Функция №3
+void write_lines_to_file(const string& output_filename, const vector<string>& lines)
+{
+
+}
+
+int main()
+{
+    string input_file = "input.txt";
+    string output_file = "output.txt";
+
+    vector<string> lines;
+
+    read_lines_from_file(input_file, lines);
+    print_lines_to_console(lines);
+    write_lines_to_file(output_file, lines);
+
     return 0;
 }
